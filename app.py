d673c33b99fb0ba9464cae4f1a96dad7a2c73f2c
ds-riselabs <labs@risenetworks.org>
2022-09-15 11:19:56 +0100

    make it good

--- app.py
+++ app.py
@@ -12,24 +12,28 @@ model = pickle.load(open('runam_model.pkl', 'rb'))
 tfidfvect = pickle.load(open('runam_tfidfvect.pkl', 'rb'))
 
 @app.route('/', methods=['GET'])
-def home():
-    return render_template('index.html')
+def index1():
+    return render_template('index1.html')
 
 def predict(text):
-    review = re.sub('[^a-zA-Z]', ' ', text)
-    review = review.lower()
-    review = review.split()
-    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
-    review = ' '.join(review)
-    review_vect = tfidfvect.transform([review]).toarray()
-    prediction = 'This news item is predicted to likely be from an unverified news source, so we advise you to be careful of what decision you make with it.' if model.predict(review_vect) == 0 else 'This news item is likely to be from a verified news source.'
+    headline = re.sub('[^a-zA-Z]', ' ', text)
+    headline = headline.lower()
+    headline = headline.split()
+    headline = [ps.stem(word) for word in headline if not word in stopwords.words('english')]
+    headline = ' '.join(headline)
+    headline_vect = tfidfvect.transform([headline]).toarray()
+    prediction = 'This news item is likely to be from an unverified news source, so be careful of what decision you make with it.' if model.predict(headline_vect) == 0 else 'This news item is likely to be from a verified news source.'
     return prediction
 
 @app.route('/', methods=['POST'])
 def webapp():
     text = request.form['text']
     prediction = predict(text)
-    return render_template('index.html', text=text, result=prediction)
+    return render_template('index1.html', text=text, result=prediction)
+
+@app.route('/about')
+def about():
+  return render_template('about.html')    
 
 
 @app.route('/predict/', methods=['GET','POST'])
